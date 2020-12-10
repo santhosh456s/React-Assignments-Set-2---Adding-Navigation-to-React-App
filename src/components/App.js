3782e59097302e43e479e98c48d73227cdcf5a56
santhosh <68135777+santhosh456s@users.noreply.github.com>
2020-12-10 20:38:43 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,37 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
-import Home from "./Home";
-import Nomatch from "./Nomatch";
-import About from "./About";
-import LocationDisplay from "./locationDisplay";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+import LocationDisplayComponent from "./LocationDisplayComponent";
+function About() {
+  return <div>You are on the about page.</div>;
+}
+function Home() {
+  return <div>You are home.</div>;
+}
+
+function Invalid() {
+  return <div>No match</div>;
+}
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <div className="navigation">
-          <a href="/">Home</a>
-          <a href="about">About</a>
+      <>
+        <div id="main">
+          {/* Do not remove the main div */}
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/" exact component={Home} />
+            <Route path="/" component={Invalid} />
+          </Switch>
+          <LocationDisplayComponent />
         </div>
-        {/* <Navigation/> */}
-        <Switch>
-          <Route path="/about" component={About} />
-          <Route path="/" component={Home} />
-          <Route component={Nomatch} />
-        </Switch>
-        <LocationDisplay />
-      </div>
+      </>
     );
   }
 }
 
 export default App;
+export const LocationDisplay = LocationDisplayComponent;
